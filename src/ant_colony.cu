b0917873d8c26b7151088ad39fa4a33153b407f8
G V Vishwanath <gvvishwanath@gmail.com>
2017-11-15 12:09:38 +0530

    Parallelised

--- src/ant_colony.cu
+++ src/ant_colony.cu
@@ -3,12 +3,11 @@
 #include <ctime>
 #include <curand_kernel.h>
 
-double PH_EVAP = 0.5;
 double ALPHA = 1;
 double BETA = 1;
 
 __device__ void gen_sol(int* path,double* cost,int n,double* gadj, double* pmap, curandState &s){
-    int *reached = (int*) malloc(n*sizeof(int));
+    int *reached = (int*) malloc(n*sizeof(int)); // reached
     for(int i=0;i<n;i++) reached[i]=0;
     int *vert= (int*) malloc(n*sizeof(int));
     double *cprob= (double*) malloc(n*sizeof(double));
@@ -56,25 +55,28 @@ __device__ void gen_sol(int* path,double* cost,int n,double* gadj, double* pmap,
     *cost += gadj[path[n-1]*n + path[0]];
 }
 
-/*
-__device__ void atomicUpdate(unsigned long long* pval_addr, double cost){
-    // update pval using cost.
-    // pval = (1-PH_EVAP)*pval + 1/cost;
-    unsigned long long old = *pval_addr, assumed;
-    do {
-        assumed = old;
-        double newval = :
-        old = atomicCAS(pval_addr, assumed, newval);
-    } while(assumed != old);
-    
-}
-*/
+__device__ void atomicUpdate (double* pval_addr, double cost)  {  
+	unsigned long long int* address_as_ull = (unsigned long long int*) pval_addr; 
+  	unsigned long long int old = *address_as_ull, assumed, newVal; 
+	double rCost = (1.0 / cost);
+	double PH_EVAP = 0.5;
+	double PH_STAY = 1 - PH_EVAP;
 
-void ph_update(int n,int* path, double* cost, double* pmap){
-    /*printf("updating: ");
-    print_vi(path, n);
-    printf(", cost: %f\n", *cost);
-    */
+  	do { 
+ 		assumed = old;
+		newVal = __double_as_longlong(rCost + PH_STAY * __longlong_as_double(assumed));
+ 		old = atomicCAS(address_as_ull, assumed, newVal); 
+  	} while (assumed != old);
+ 
+	//return __longlong_as_double(old);
+}      
+
+
+/*void ph_update(int n,int* path, double* cost, double* pmap){
+    //printf("updating: ");
+    //print_vi(path, n);
+    //printf(", cost: %f\n", *cost);
+    
     int v = path[0],u;
     for(int i=1; i<n;i++){
         u = path[i];
@@ -86,11 +88,24 @@ void ph_update(int n,int* path, double* cost, double* pmap){
     u = 0;
     //printf("update edge %d-%d\n",v,u);
     pmap[v*n+u] = (1- PH_EVAP)*pmap[v*n+u] + 1/(*cost);
+}*/
+
+
+__device__ void ph_update (int n,int* path, double cost, double* pmap){
+    //printf("updating: ");
+    //print_vi(path, n);
+    //printf(", cost: %f\n", cost);
+    
+    int i;
+    for (i = 0; i < n-1; i++) {
+        atomicUpdate(&pmap[path[i] * n + path[i+1]], cost);
+    }
+    atomicUpdate(&pmap[path[n-1] * n], cost);
 }
 
 
 
-__global__ void test_k(int n, int *paths, double* costs, double *gadj, double *p, int seed){
+__global__ void test_k(int n, int *paths, double* costs, double *gadj, double *p, int seed, double *cost, int *mini, int *path){
     int i = threadIdx.x;
     //printf("id: %d %d\n",i, n);
     if(i >=n ) return;
@@ -104,6 +119,36 @@ __global__ void test_k(int n, int *paths, double* costs, double *gadj, double *p
     gen_sol(rpath, rcost, n,gadj, p, s);
     //printf("id %d cost %f cost' %f\n",i,*rcost,costs[i]);
 
+    
+    unsigned long long oldCost, assumedCost, newCost;
+    int oldMini, assumedMini, newMini;
+    
+    if(*rcost < *cost){
+	unsigned long long *ullCost = (unsigned long long *) cost;
+	oldCost = *ullCost;
+	do { 
+ 	  assumedCost = oldCost;
+	  newCost = __double_as_longlong(*rcost);
+ 	  oldCost = atomicCAS(ullCost, assumedCost, newCost); 
+  	} while (assumedCost != oldCost);
+
+	oldMini = *mini;
+	do { 
+ 	  assumedMini = oldMini;
+	  newMini = i;
+ 	  oldMini = atomicCAS(mini, assumedMini, newMini); 
+  	} while (assumedMini != oldMini);
+    }
+    __syncthreads();
+
+    if (*mini != -1) {
+       if(i < n) {
+            path[i]=paths[(*mini)*n+i];
+       }
+    }
+    __syncthreads();
+
+    ph_update(n, rpath, *rcost, p);
 }
 
 
@@ -122,53 +167,96 @@ int ant_colony_opt_tsp(int n, double* gadj, int *path, double &cost)
     double *p = new double[n*n];
     for(int i=0;i<n*n;i++) p[i]=1e-5;
     cost=1.0e10;
+
+    int *paths = new int[NA*n]; // paths[ant] = ant's path
+    double *costs = new double[NA];
+    int *paths_d;
+    double *costs_d, *gadj_d, *p_d;
+    double *cost_d;
+    int *mini_d;
+    int *path_d;
+
+    cudaMalloc(&paths_d, NA*n*sizeof(int));
+    cudaMalloc(&costs_d, NA*sizeof(double));
+    cudaMalloc(&gadj_d, n*n*sizeof(double));
+    cudaMalloc(&p_d, n*n*sizeof(double));
+    cudaMalloc(&cost_d, sizeof(double));
+    cudaMalloc(&mini_d, sizeof(double));
+    cudaMalloc(&path_d, n*sizeof(int));
+
+    cudaMemcpy(gadj_d, gadj, n*n*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(p_d, p, n*n*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy(cost_d, &cost, sizeof(double), cudaMemcpyHostToDevice);
+
     for (int iter = 0; iter < NI; iter++)
     {
-        int *paths = new int[NA*n]; // paths[ant] = ant's path
+        /*int *paths = new int[NA*n]; // paths[ant] = ant's path
         double *costs = new double[NA];
         int *paths_d;
         double *costs_d, *gadj_d, *p_d;
+	double *cost_d;
+
         cudaMalloc(&paths_d, NA*n*sizeof(int));
         cudaMalloc(&costs_d, NA*sizeof(double));
         cudaMalloc(&gadj_d, n*n*sizeof(double));
         cudaMalloc(&p_d, n*n*sizeof(double));
+	cudaMalloc(&cost_d, sizeof(double));
 
         cudaMemcpy(gadj_d, gadj, n*n*sizeof(double), cudaMemcpyHostToDevice);
-        cudaMemcpy(p_d, p, n*n*sizeof(double), cudaMemcpyHostToDevice);
+        cudaMemcpy(p_d, p, n*n*sizeof(double), cudaMemcpyHostToDevice);*/
 
-        test_k<<<1,NA>>>(n,paths_d, costs_d, gadj_d, p_d,iter);
+	int mini = -1;
+	cudaMemcpy(mini_d, &mini, sizeof(int), cudaMemcpyHostToDevice);
+
+        test_k<<<1,NA>>>(n,paths_d, costs_d, gadj_d, p_d,iter, cost_d, mini_d, path_d);
 
         cudaThreadSynchronize();
 
-        cudaMemcpy(paths, paths_d, NA*n*sizeof(int), cudaMemcpyDeviceToHost);
-        cudaMemcpy(costs, costs_d, NA*sizeof(double), cudaMemcpyDeviceToHost);
+        //cudaMemcpy(paths, paths_d, NA*n*sizeof(int), cudaMemcpyDeviceToHost);
+        //cudaMemcpy(costs, costs_d, NA*sizeof(double), cudaMemcpyDeviceToHost);
+	//cudaMemcpy(&cost, cost_d, sizeof(double), cudaMemcpyDeviceToHost);
+
         
-        for(int i=0;i<0;i++){
+        /*for(int i=0;i<0;i++){
             print_vi(paths+i*n,n);
             printf(", cost=%f\n",costs[i]);
-        }
+        }*/
 
-        int mini=-1;
-        for (int i= 0; i< NA; i++)
+        /*for (int i= 0; i< NA; i++)
         {
-            int *rpath = paths+i*n;
             double *rcost = costs+i;
             if(*rcost < cost){
                 mini=i;
-                cost = *rcost;
             }   
-            ph_update(n,rpath, rcost, p);
-        }
+        */
+
+	/*cudaMemcpy(&mini, mini_d, sizeof(int), cudaMemcpyDeviceToHost);
         if(mini!=-1){
             for(int i=0;i<n;i++) path[i]=paths[mini*n+i];
-        }
-        delete[] paths;
+        }*/
+
+
+        /*delete[] paths;
         delete[] costs;
         cudaFree(gadj_d);
         cudaFree(p_d);
         cudaFree(paths_d);
         cudaFree(costs_d);
+	cudaFree(cost_d);*/
     }
+
+    cudaMemcpy(path, path_d, n*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(&cost, cost_d, sizeof(double), cudaMemcpyDeviceToHost);
+
+    delete[] paths;
+    delete[] costs;
+    cudaFree(gadj_d);
+    cudaFree(p_d);
+    cudaFree(paths_d);
+    cudaFree(costs_d); 
+    cudaFree(cost_d);
+    cudaFree(path_d);
+
     delete[] p;
     clock_t en = clock();
     double t = (double)(en-st)/CLOCKS_PER_SEC;
